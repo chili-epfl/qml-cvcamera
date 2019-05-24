0649dc9e1253120cd33713b79c7093c05e53ae22
julianguarin <julian.guarin.2010@gmail.com>
2019-05-24 01:31:35 -0500

    preview sample depicting QObject injection to the QML Context rather than Instantiation in the QML script

--- samples/preview/src/main.cpp
+++ samples/preview/src/main.cpp
@@ -1,3 +1,6 @@
+#include <QSize>
+#include <CVCamera.h>
+#include <QQmlContext>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
@@ -6,6 +9,16 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    //Instantiate Camera.
+    CVCamera camera;
+    camera.setSize(QSize(640,480));
+
+
+
+    auto qmlContext = engine.rootContext();
+    qmlContext->setContextProperty("camerax", &camera);
+
     engine.load(QUrl(QStringLiteral("qrc:/src/main.qml")));
 
     return app.exec();
